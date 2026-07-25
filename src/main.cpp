895ae88d5d3bff3a4ccd34fbf7d574afe091aeda
CloudCodingSpace <subhamaygamer2127@outlook.com>
2026-07-25 21:51:57 +0530

    Some more important changes to the client and the test server

--- src/main.cpp
+++ src/main.cpp
@@ -17,24 +17,24 @@ int main() {
     std::cin >> port;
 
     WSADATA data{};
-    WSAStartup(MAKEWORD(2, 2), &data);
+    SOCKET client{};
+    
+    // WinSock setup
+    {
+        WSAStartup(MAKEWORD(2, 2), &data);
 
-    SOCKET client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    sockaddr_in addr{};
-    addr.sin_addr.s_addr = inet_addr(ip.c_str());
-    addr.sin_port = htons(port);
-    addr.sin_family = AF_INET;
+        sockaddr_in addr{};
+        addr.sin_addr.s_addr = inet_addr(ip.c_str());
+        addr.sin_port = htons(port);
+        addr.sin_family = AF_INET;
 
-    if(connect(client, (sockaddr*)&addr, sizeof(addr)) != 0) {
-        std::cerr << "Failed to connect to server!" << std::endl;
-        return -1;
+        if(connect(client, (sockaddr*)&addr, sizeof(addr)) != 0) {
+            std::cerr << "Failed to connect to server!" << std::endl;
+            return -1;
+        }
     }
-
-    send(client, "Yooo!!", 6 * sizeof(char), 0);
-
-    closesocket(client);
-    WSACleanup();
 
     // Window
     assert(glfwInit() && "Failed to init glfw!");
@@ -68,6 +68,14 @@ int main() {
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
+
+    send(client, "/q", 2 * sizeof(char), 0);
+
+    closesocket(client);
+    WSACleanup();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
--- test_server.py
+++ test_server.py
@@ -5,14 +5,13 @@ server.bind(("0.0.0.0", 8080))
 server.listen(1)
 
 client, addr = server.accept()
+print("Client connected!")
 
 while True:
     data = client.recv(1024)
-    if not data:
-        print("Client disconnected")
-    else:
-        decodedData = data.decode()
+    decodedData = data.decode()
 
-        if decodedData == '/q':
-            break
-        print(decodedData)
+    if decodedData == '/q':
+        print("Client disconnecting ... Shutting down server!")
+        break
+    print(decodedData)
